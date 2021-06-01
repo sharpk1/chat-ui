369c2131d4858e264ea539bd8d21e283b59a5a66
Kush Shah <kshah@lwolf.com>
2021-05-31 20:22:50 -0500

    add bootstrap, textbox, and button

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -3,7 +3,22 @@ import { UserContext } from "../UserContext";
 
 const Chatroom = () => {
   const { user, setUser } = useContext(UserContext);
-  return <>{user}</>;
+  return (
+    <>
+      {user}
+      <div class="message-container">
+      <input
+        class="form-control form-control-sm"
+        type="text"
+        placeholder="Type your message here."
+      />
+      <button type="button" class="btn btn-primary">
+        Send
+      </button>
+      </div>
+      
+    </>
+  );
 };
 
 export default Chatroom;
